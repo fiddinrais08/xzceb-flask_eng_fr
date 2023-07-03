8692bdb97e4098d46992649706349c83be673e3f
fiddinrais08 <fiddin.rais08@yahoo.de>
2023-07-03 08:33:21 -0400

    Another Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,10 +4,10 @@ from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french('Hello'), 'Pepitoooo')
+        self.assertEqual(english_to_french('hello'), 'bonjour')
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertEqual(french_to_english('Pepitoooo'), 'Hello')
+        self.assertEqual(french_to_english('bonjour'), 'hello')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -7,12 +7,12 @@ def english_to_french(english_text):
     """
     This function translate English to French
     """
-    french_text = Mem(source='en-GB', target='fr-FR').translate(english_text)
+    french_text = Mem(source='en', target='fr').translate(english_text)
     return french_text
 
 def french_to_english(french_text):
     """
     This function translate French to English
     """
-    english_text = Mem(source='fr-FR', target='en-GB').translate(french_text)
+    english_text = Mem(source='fr', target='en').translate(french_text)
     return english_text
